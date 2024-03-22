f065b5ca3523bf077c47bcf5c0060dbec401d733
yy6768 <102475442+yy6768@users.noreply.github.com>
2024-03-22 10:50:45 +0800

    Revert "药品增删改的接口实现"

--- backend/src/main/java/com/example/petbackend/controller/medicine/MedicineController.java
+++ backend/src/main/java/com/example/petbackend/controller/medicine/MedicineController.java
@@ -14,37 +14,11 @@ public class MedicineController {
     @Autowired
     private MedicineService medicineService;
 
-    /**
-     * 添加一个药品
-     * @param map
-     */
-    @PostMapping("/medications/add/")
+    @PostMapping
     public Map<String, String> addMedicine(@RequestParam Map<String, String> map){
         String medicine_name = map.get("medicine_name");
-        Double medicine_cost= Double.valueOf(map.get("medicine_cost"));
+        Integer medicine_cost= Integer.valueOf(map.get("medicine_cost"));
+
         return medicineService.addMedicine(medicine_name, medicine_cost);
     }
-
-    /**
-     * 修改一个药品费用
-     * @param map
-     */
-    @PostMapping("/medications/update/")
-    public Map<String, String> updateMedicine(@RequestParam Map<String, String> map){
-        Integer medicine_id = Integer.valueOf(map.get("medicine_id"));
-        Double medicine_cost = Double.valueOf(map.get("medicine_cost"));
-        return medicineService.updateMedicine(medicine_id, medicine_cost);
-    }
-
-    /**
-     * 删除一个药品
-     * @param map
-     */
-    @PostMapping("/medications/delete/")
-    public Map<String, String> deleteMedicine(@RequestParam Map<String, String> map){
-        Integer medicine_id = Integer.valueOf(map.get("medicine_id"));
-        return medicineService.deleteMedicine(medicine_id);
-    }
-
-
 }
--- backend/src/main/java/com/example/petbackend/service/impl/medicine/MedicineServiceImpl.java
+++ backend/src/main/java/com/example/petbackend/service/impl/medicine/MedicineServiceImpl.java
@@ -1,9 +1,6 @@
 package com.example.petbackend.service.impl.medicine;
 
-import com.example.petbackend.mapper.MedicineMapper;
-import com.example.petbackend.pojo.Medicine;
 import com.example.petbackend.service.medicine.MedicineService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -11,50 +8,14 @@ import java.util.Map;
 
 @Service
 public class MedicineServiceImpl implements MedicineService {
-
-    @Autowired
-    MedicineMapper medicineMapper;
-
-    //添加药品
     @Override
-    public Map<String, String> addMedicine(String medicine_name, Double medicine_cost){
-        Medicine medicine = new Medicine(medicine_name, medicine_cost);
-        medicineMapper.insert(medicine);
-        Map<String,String> medicineMap=new HashMap<>();
-        medicineMap.put("error_message", "success");
-        medicineMap.put("medicine_id", String.valueOf(medicineMapper.insert(medicine)));
-        return medicineMap;
+    public Map<String, String> addMedicine(String medicine_name, Integer medicine_cost){
+        return null;
     };
-
-    //修改药品费用
-    @Override
-    public Map<String, String> updateMedicine(Integer medicine_id, Double medicine_cost){
-        Medicine medicine = medicineMapper.selectById(medicine_id);
-        if(medicine!=null){
-            medicine.setMedicine_cost(medicine_cost);
-            medicineMapper.updateById(medicine);
-        }
-        Map<String,String> medicineMap=new HashMap<>();
-        if(medicineMapper.updateById(medicine) < 1){
-            medicineMap.put("error_message", "failure");
-        }
-        else{
-            medicineMap.put("error_message", "success");
-        }
-        return medicineMap;
+    public Map<String, String> updateMedicine(Integer medicine_id, Integer medicine_cost){
+        return null;
     };
-
-    //删除药品
-    @Override
     public Map<String, String> deleteMedicine(Integer medicine_id){
-        medicineMapper.deleteById(medicine_id);
-        Map<String,String> medicineMap=new HashMap<>();
-        if(medicineMapper.deleteById(medicine_id) < 1){
-            medicineMap.put("error_message", "failure");
-        }
-        else{
-            medicineMap.put("error_message", "success");
-        }
-        return medicineMap;
+        return null;
     };
 }
