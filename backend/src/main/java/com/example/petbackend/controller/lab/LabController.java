0c5e1f76652c28f09aa04d7a400e2e249c43fc41
jiangxiansheng <1874813131@qq.com>
2024-03-25 12:49:14 +0800

    2

--- backend/src/main/java/com/example/petbackend/controller/lab/LabController.java
+++ backend/src/main/java/com/example/petbackend/controller/lab/LabController.java
@@ -22,10 +22,9 @@ public class LabController {
     @PutMapping("/lab/{lab_id}/")
     public Map<String,String> updateLab(@RequestParam Map<String,String> map){
         Integer lab_id= Integer.valueOf(map.get("lab_id"));
-        String lab_name=map.get("lab_name");
         Double lab_cost= Double.valueOf(map.get("lab_cost"));
 
-        return labService.updateLab(lab_id,lab_name,lab_cost);
+        return labService.updateLab(lab_id,lab_cost);
     }
 
     @DeleteMapping("/lab/{lab_id}/")
--- backend/src/main/java/com/example/petbackend/service/lab/LabService.java
+++ backend/src/main/java/com/example/petbackend/service/lab/LabService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public interface LabService {
     Map<String,String> addLab(String lab_name,Double lab_cost);
 
-    Map<String,String> updateLab(Integer lab_id,String lab_name,Double lab_cost);
+    Map<String,String> updateLab(Integer lab_id,Double lab_cost);
 
     Map<String,String> deleteLab(Integer lab_id);
 }
