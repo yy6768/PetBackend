ffed7660c56823e9250a3e2908cf256f58be9089
yy6768 <1258720926@qq.com>
2024-03-29 10:12:12 +0800

    Update user search

--- backend/src/main/java/com/example/petbackend/service/impl/user/LoginServiceImpl.java
+++ backend/src/main/java/com/example/petbackend/service/impl/user/LoginServiceImpl.java
@@ -37,13 +37,15 @@ public class LoginServiceImpl implements LoginService {
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);//登录失败自动处理
 
         UserDetailsImpl loginUser = (UserDetailsImpl) authenticate.getPrincipal();
-        User user = loginUser.getUser();
-
-        String jwt = JwtUtil.createJWT(user.getUid().toString());
-
         Map<String,String> map=new HashMap<>();
+        User user = loginUser.getUser();
+        if (user.getAuthority() == 0) {
+            map.put("error_message", "用户已被禁用");
+        }
+        String jwt = JwtUtil.createJWT(user.getUid().toString());
         map.put("error_message","success");
         map.put("token",jwt);
+        map.put("authority", user.getAuthority().toString());
         return map;
     }
 
