ffaac12bb35ea105580dd15721bc95206cd541d2
huyunshu <2551814945@qq.com>
2024-04-10 15:05:49 +0800

    update

--- backend/src/main/java/com/example/petbackend/controller/paper/PaperController.java
+++ backend/src/main/java/com/example/petbackend/controller/paper/PaperController.java
@@ -1,9 +1,9 @@
 package com.example.petbackend.controller.paper;
 
+import com.alibaba.fastjson.JSON;
 import com.example.petbackend.service.paper.PaperService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,22 +22,20 @@ public class PaperController {
 
     //添加试卷
     @PostMapping("/paper/add")
-    public Map<String, String> addPaper(@RequestParam Map<String, String> map) throws ParseException{
-        Integer uid = Integer.valueOf(map.get("uid"));
-        String paper_name = map.get("paper_name");
-        Timestamp time = Timestamp.valueOf(map.get("time"));
-        Date date = (Date) new SimpleDateFormat("yyyy-MM-dd").parse(map.get("date"));
-        // 将question_list字符串转换为数组
-        String questionListStr = map.get("question_list");
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            Integer[] question_list = objectMapper.readValue(questionListStr, Integer[].class);
-            return paperService.addPaper(uid, paper_name, time, date, question_list);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            // 处理 JSON 解析异常
-            return null; // 或者抛出自定义异常，根据业务需求处理
+    public Map<String, String> addPaper(@RequestParam Map<String, Object> map) throws ParseException{
+        Integer uid = Integer.valueOf((String) map.get("uid"));
+        String paper_name = (String) map.get("paper_name");
+        Timestamp time = Timestamp.valueOf((String) map.get("time"));
+        Date date = (Date) new SimpleDateFormat("yyyy-MM-dd").parse((String) map.get("date"));
+
+        // 将题目列表字符串转换为数组
+        String questionListStr = (String) map.get("question_list");
+        String[] questionListArray = questionListStr.split(",");
+        Integer[] question_list = new Integer[questionListArray.length];
+        for (int i = 0; i < questionListArray.length; i++) {
+            question_list[i] = Integer.parseInt(questionListArray[i].trim());
         }
+        return paperService.addPaper(uid, paper_name, time, date, question_list);
     }
 
     //删除试卷
