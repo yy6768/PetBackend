d4842b403212928dde28cfea673e791bd8020229
yy6768 <1258720926@qq.com>
2024-04-17 15:11:20 +0800

    Init Spring redis & update config

--- backend/src/main/java/com/example/petbackend/utils/ExamRedisUtil.java
+++ backend/src/main/java/com/example/petbackend/utils/ExamRedisUtil.java
@@ -1,12 +1,11 @@
 package com.example.petbackend.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -17,17 +16,19 @@ public class ExamRedisUtil {
      * 如果你使用其他类型的 比如RedisTemplate<String,Object>
      * 那么用 @Resource 注解
      * */
-    @Autowired
-    private RedisTemplate<String,String> redisTemplate;
+    private final RedisTemplate<String,String> redisTemplate;
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ExamRedisUtil(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     /**
      * 缓存value，有设置超时时间
-     * @param key
-     * @param value
-     * @param time
-     * @return
+     * @param key 键
+     * @param value 值
+     * @param time 超时时间
      */
     public boolean cacheValue(String key, String value, long time) {
         try {
@@ -46,9 +47,8 @@ public class ExamRedisUtil {
 
     /**
      * 缓存value，没有设置超时时间
-     * @param key
-     * @param value
-     * @return
+     * @param key 键
+     * @param value 值
      */
     public boolean cacheValue(String key, String value) {
         return cacheValue(key, value, -1);
@@ -56,12 +56,11 @@ public class ExamRedisUtil {
 
     /**
      * 判断缓存是否存在
-     * @param key
-     * @return
+     * @param key 键
      */
     public boolean containsKey(String key) {
         try {
-            return redisTemplate.hasKey(key);
+            return Boolean.TRUE.equals(redisTemplate.hasKey(key));
         } catch (Throwable e) {
             logger.error("判断缓存是否存在时失败key[" + key + "]", "err[" + e.getMessage() + "]");
         }
@@ -70,8 +69,7 @@ public class ExamRedisUtil {
 
     /**
      * 根据key，获取缓存
-     * @param key
-     * @return
+     * @param key 键
      */
     public String getValue(String key) {
         try {
@@ -85,8 +83,7 @@ public class ExamRedisUtil {
 
     /**
      * 移除缓存
-     * @param key
-     * @return
+     * @param key 键
      */
     public boolean removeValue(String key) {
         try {
@@ -100,13 +97,14 @@ public class ExamRedisUtil {
 
     /**
      * 根据前缀移除所有以传入前缀开头的key-value
-     * @param pattern
-     * @return
+     * @param pattern 正则表达式
      */
     public boolean removeKeys(String pattern) {
         try {
             Set<String> keySet = redisTemplate.keys(pattern + "*");
-            redisTemplate.delete(keySet);
+            if (keySet != null) {
+                redisTemplate.delete(keySet);
+            }
             return true;
         } catch (Throwable e) {
             logger.error("移除key[" + pattern + "]前缀的缓存时失败", "err[" + e.getMessage() + "]");
