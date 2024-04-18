3c9f8ac633f2ddf789f5babdcf6c12520d8074b6
huyunshu <2551814945@qq.com>
2024-04-18 14:02:42 +0800

    init实习生根据uid获取考试接口

--- backend/src/main/java/com/example/petbackend/controller/exam/GetExamController.java
+++ backend/src/main/java/com/example/petbackend/controller/exam/GetExamController.java
@@ -15,7 +15,7 @@ public class GetExamController {
     GetExamService getExamService;
 
     /**
-     * 根据考试名搜索分页获取所有考试
+     * 管理员根据考试名搜索分页获取所有考试
      * @param map
      * @return
      */
@@ -28,7 +28,7 @@ public class GetExamController {
     }
 
     /**
-     * 根据ID获取考试详情
+     * 管理员根据ID获取考试详情
      * @param map
      * @return
      */
@@ -36,6 +36,19 @@ public class GetExamController {
     public Map<String, Object> getExamById(@RequestParam Map<String, String> map){
         Integer exam_id = Integer.valueOf(map.get("exam_id"));
         return getExamService.getExamById(exam_id);
+    }
+
+    /**
+     * 实习生根据uid获取其能参与的考试
+     * @param map
+     * @return
+     */
+    @GetMapping("/exam/getByUid")
+    public Map<String, Object> getExamByUid(@RequestParam Map<String, String> map){
+        Integer page = Integer.valueOf(map.get("page"));
+        Integer pageSize = Integer.valueOf(map.get("pageSize"));
+        Integer uid = Integer.valueOf(map.get("uid"));
+        return getExamService.getExamByUid(uid, page, pageSize);
     }
 
 
--- backend/src/main/java/com/example/petbackend/service/impl/exam/GetExamServiceImpl.java
+++ backend/src/main/java/com/example/petbackend/service/impl/exam/GetExamServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.petbackend.dto.ExamDTO;
+import com.example.petbackend.dto.ExamUserDTO;
 import com.example.petbackend.mapper.*;
 import com.example.petbackend.pojo.*;
 import com.example.petbackend.service.exam.GetExamService;
@@ -123,6 +124,61 @@ public class GetExamServiceImpl implements GetExamService {
             examMap.put("error_msg", "未找到对应考试");
         }
 
+        return examMap;
+    }
+
+    //实习生根据uid获取其能参加的考试
+    public Map<String, Object> getExamByUid(Integer uid, Integer page, Integer pageSize){
+        QueryWrapper<ExamUser> examUserQueryWrapper = new QueryWrapper<>();
+        examUserQueryWrapper.eq("uid", uid);
+        List<ExamUser> examUserList = examUserMapper.selectList(examUserQueryWrapper);
+        List<ExamUserDTO> examUserDTOList = new ArrayList<>();
+        for(ExamUser examUser: examUserList){
+            ExamUserDTO examUserDTO = new ExamUserDTO();
+            examUserDTO.setEu_id(examUser.getEuId());
+            examUserDTO.setExam_id(examUser.getExamId());
+            examUserDTO.setGrade(examUser.getGrade());
+            //查询exam_name,begin_time
+            Exam exam = examMapper.selectById(examUser.getExamId());
+            examUserDTO.setExam_name(exam.getExamName());
+            examUserDTO.setBeginTime(exam.getBeginTime());
+            //查询time，计算end_time
+            Paper paper = paperMapper.selectById(exam.getPaperId());
+            examUserDTO.setTime(paper.getTime());
+            LocalDateTime time = paper.getTime().toLocalDateTime();
+            LocalDateTime endTime = exam.getBeginTime().plusHours(time.getHour())
+                    .plusMinutes(time.getMinute())
+                    .plusSeconds(time.getSecond())
+                    .minusHours(8);
+            examUserDTO.setEndTime(endTime);
+            //计算考试总分
+            QueryWrapper<PaperQuestion> paperQuestionQueryWrapper = new QueryWrapper<>();
+            paperQuestionQueryWrapper.eq("paper_id", paper.getPaperId());
+            List<PaperQuestion> paperQuestionList = paperQuestionMapper.selectList(paperQuestionQueryWrapper);
+            Integer totalMark = 0;
+            for(PaperQuestion paperQuestion : paperQuestionList){
+                Integer qid = paperQuestion.getQid();
+                Question question = questionMapper.selectById(qid);
+                totalMark = totalMark + question.getMark();
+            }
+            examUserDTO.setTotalMark(totalMark);
+            //插入列表
+            examUserDTOList.add(examUserDTO);
+        }
+        //分页
+        int totalSize = examUserDTOList.size();
+        int fromIndex = (page - 1) * pageSize;
+        int toIndex = Math.min(fromIndex + pageSize, totalSize);
+        List<ExamUserDTO> pageList = examUserDTOList.subList(fromIndex, toIndex);
+        //返回
+        Map<String, Object> examMap = new HashMap<>();
+        if(examUserDTOList != null && !examUserDTOList.isEmpty()){
+            examMap.put("error_msg", "success");
+            examMap.put("exam_list", pageList);
+            examMap.put("total", totalSize);
+        } else{
+            examMap.put("error_msg", "考生没有可参与的考试");
+        }
         return examMap;
     }
 
