f7b03d1f708e8c14b10308798958ee6a0cbad71a
yy6768 <1258720926@qq.com>
2024-04-20 23:30:17 +0800

    修复结束考试的逻辑问题

---
+++ backend/src/main/java/com/example/petbackend/dto/QuestionOfExamDTO.java
@@ -0,0 +1,13 @@
+package com.example.petbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionOfExamDTO {
+    private Integer num;
+    private Integer qid;
+}
